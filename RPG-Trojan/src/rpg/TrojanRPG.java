9ca8254788b6c05fca545de1ec4b622ed6162aba
Gessione <71856400+Gessione@users.noreply.github.com>
2020-11-07 16:00:39 -0300

    Update TrojanRPG.java

--- RPG-Trojan/src/rpg/TrojanRPG.java
+++ RPG-Trojan/src/rpg/TrojanRPG.java
@@ -1,7 +1,68 @@
 package rpg;
 
+import java.util.Scanner;
+
 public class TrojanRPG {
-	long milisPorChar = 85;
+	static int vida = 5;
+	static long milisPorChar = 85;
+	static Scanner entrada = new Scanner (System.in);
+	
+	public static void main(String[]args)throws Exception{
+		Menu();
+		
+		}
+	public static void perdeVida(int hit){
+		if(hit>=vida) {			
+			System.out.println("GameOver");
+			//FIM DO JOGO//		
+		}
+		else{vida=vida-hit;}
+			}
+	public static void bonusDeVida(int extra) {
+		if(extra>0) {
+			vida=vida+extra;
+		}
+		
+	}
+	public static void Menu()throws Exception{
+		boolean repitir = false;
+		do {
+		digita("1 Jogar\n",milisPorChar);
+		digita("2 Opções\n",milisPorChar);
+		digita("3 Creditos\n",milisPorChar);
+		digita("4 Sair\n",milisPorChar);
+		int n1 = entrada.nextInt();
+		
+		switch(n1) {
+		
+		case 1: 
+		// Iniciar o jogo//
+		break;
+		
+		case 2:
+		// Abrir as opções//
+		break;
+		
+		case 3:
+		System.out.println("Esse jogo foi desenvolvido por:\n Rodolpho Alcantra \n Felipe Fabrini \n Gessione Freitas" );
+		break;
+			
+		case 4:
+			digita("Saindo...",milisPorChar);
+		System.exit(1);
+		break;
+		
+		default:
+			System.out.print("\n Não tem essa opção !!!\n");
+			System.out.print(" Tente novamente\n\n");
+			repitir= true;
+			break;
+		
+		}
+		
+		}while(repitir);
+		
+	}
 	/**
 	 * Método que imprime na tela aos poucos. Funciona como uma máquina de escrever.
 	 * 
