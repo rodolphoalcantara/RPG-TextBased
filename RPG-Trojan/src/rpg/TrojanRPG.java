1df07febeb23299a4660512529b65a267eeddc32
Rodolpho Alcantara <rodolphoalcantara@outlook.com>
2020-11-08 15:28:33 -0300

    Adição do metodo opcoes que altera a velocidade do texto, correcao de algumas linhas.

--- RPG-Trojan/src/rpg/TrojanRPG.java
+++ RPG-Trojan/src/rpg/TrojanRPG.java
@@ -3,71 +3,162 @@ package rpg;
 import java.util.Scanner;
 
 public class TrojanRPG {
+	// variaveis "globais"
 	static int vida = 5;
-	static long milisPorChar = 85;
-	static Scanner entrada = new Scanner (System.in);
-	
-	public static void main(String[]args)throws Exception{
-		Menu();
-		
-		}
-	public static void perdeVida(int hit){
-		if(hit>=vida) {			
-			System.out.println("GameOver");
-			//FIM DO JOGO//		
-		}
-		else{vida=vida-hit;}
-			}
-	public static void bonusDeVida(int extra) {
-		if(extra>0) {
-			vida=vida+extra;
-		}
-		
+	static long milisPorChar = 65;
+	static Scanner entrada = new Scanner(System.in);
+
+	public static void main(String[] args) throws Exception {
+		menu();
+
 	}
-	public static void Menu()throws Exception{
-		boolean repitir = false;
+
+	// métodos do jogo
+
+	public static void menu() throws Exception {
+		boolean repetir = false;
 		do {
-		digita("1 Jogar\n",milisPorChar);
-		digita("2 Opções\n",milisPorChar);
-		digita("3 Creditos\n",milisPorChar);
-		digita("4 Sair\n",milisPorChar);
-		int n1 = entrada.nextInt();
-		
-		switch(n1) {
-		
-		case 1: 
-		// Iniciar o jogo//
-		break;
-		
-		case 2:
-		// Abrir as opções//
-		break;
-		
-		case 3:
-		System.out.println("Esse jogo foi desenvolvido por:\n Rodolpho Alcantra \n Felipe Fabrini \n Gessione Freitas" );
-		break;
-			
-		case 4:
-			digita("Saindo...",milisPorChar);
-		System.exit(1);
-		break;
-		
-		default:
-			System.out.print("\n Não tem essa opção !!!\n");
-			System.out.print(" Tente novamente\n\n");
-			repitir= true;
-			break;
-		
-		}
-		
-		}while(repitir);
-		
+			digita("1 Jogar\n", milisPorChar);
+			digita("2 Opções\n", milisPorChar);
+			digita("3 Creditos\n", milisPorChar);
+			digita("4 Sair\n", milisPorChar);
+			int n1 = entrada.nextInt();
+
+			switch (n1) {
+
+			case 1:
+				// Iniciar o jogo//
+				break;
+
+			case 2:
+				opcao();
+				break;
+
+			case 3:
+				digita("Esse jogo foi desenvolvido por:\n Rodolpho Alcantara \n Felipe Fabrini \n Gessione Freitas\n\n",
+						60);
+				menu();
+				break;
+
+			case 4:
+				digita("Saindo...", milisPorChar);
+				System.exit(1);
+				break;
+
+			default:
+				System.out.print("\n Não tem essa opção !!!\n");
+				System.out.print(" Tente novamente\n\n");
+				repetir = true;
+				break;
+
+			}
+
+		} while (repetir);
+
 	}
+
+	// opções: usuário escolhe a velocidade do texto
+	public static void opcao() throws Exception {
+		boolean sair = false, aceitou = false;
+		do {
+			digita("Escolha a velocidade de digitação.\n", milisPorChar);
+			digita("1 Rápido\n", milisPorChar);
+			digita("2 Normal\n", milisPorChar);
+			digita("3 Devagar\n", milisPorChar);
+			digita("4 Sair\n", milisPorChar);
+			int option = entrada.nextInt();
+			switch (option) {
+			case 1:
+				milisPorChar = 35;
+				digita("Alterado para rápido!\n", milisPorChar);
+				break;
+			case 2:
+				milisPorChar = 65;
+				digita("Alterado para normal!\n", milisPorChar);
+				break;
+			case 3:
+				milisPorChar = 100;
+				digita("Alterado para devagar!\n", milisPorChar);
+				break;
+			case 4:
+				digita("Saindo das opções...\n", milisPorChar);
+				menu();
+				break;
+
+			default:
+				digita("Valor incorreto será utilizado o valor padrão! \n", milisPorChar);
+				milisPorChar = 85;
+				break;
+			}
+
+			digita("Teste: ", milisPorChar);
+			digita("Este é um jogo que faz parte de um projeto da Faculdade Senac, desenvolvido pelos alunos \n"
+					+ "Felipe Fabrini, Gessione Freitas e Rodolpho Alcântara. No jogo, você interpreta o papel de um jovem estudioso \n"
+					+ "que ao instalar um vírus, entra no computador e precisa desintalar esse software malicioso para sair da maquina.\n",
+					milisPorChar);
+			do {
+				digita("Esta velocidade está boa? \n", milisPorChar);
+				digita("1 SIM\n", milisPorChar);
+				digita("2 NÃO\n", milisPorChar);
+				int aceita = entrada.nextInt();
+				switch (aceita) {
+				case 1:
+					digita("Vamos voltar para o menu!\n", milisPorChar);
+					aceitou = true;
+					sair = true;
+					break;
+				case 2:
+					digita("Vamos tentar selecionar a velocidade de novo...\n", milisPorChar);
+					aceitou = true;
+					break;
+
+				default:
+					digita("Não entendi! Tente novamente...\n", milisPorChar);
+					break;
+				}
+
+			} while (!aceitou);
+
+		} while (!sair);
+		menu();
+	}
+
+	// métodos de funcionalidades do game
+
+	/**
+	 * Método que retira quantidade de vidas do total do jogador.
+	 * 
+	 * @param hit Recebe um Int equivalente a quantidade de hits que irá tirar da
+	 *            vida total
+	 */
+	public static void perdeVida(int hit) {
+		if (hit >= vida) {
+			System.out.println("GameOver");
+			// FIM DO JOGO//
+		} else {
+			vida = vida - hit;
+		}
+	}
+
+	/**
+	 * Método que adiciona um bonus de vida ao valor atual do player
+	 *
+	 * @param extra Recebe um Int equivalente a quantidade de vidas que o player irá
+	 *              receber
+	 */
+	public static void bonusDeVida(int extra) {
+		if (extra > 0) {
+			vida = vida + extra;
+		}
+
+	}
+
 	/**
 	 * Método que imprime na tela aos poucos. Funciona como uma máquina de escrever.
 	 * 
-	 * @param mensagem Uma string que deseja imprimir na tela
-	 * @param milisPorChar Velocidade que os caracteres serão impressos na tela ("Velocidade da digitação")
+	 * @param mensagem     Uma string que deseja imprimir na tela
+	 * @param milisPorChar Velocidade que os caracteres serão impressos na tela
+	 *                     ("Velocidade da digitação")
 	 * @throws InterruptedException
 	 */
 	public static void digita(String mensagem, long milisPorChar) throws InterruptedException {
